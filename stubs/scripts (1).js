d49c00b2cd2c615c9b9fc996a305b8a299b58168
Cosmin Gheorghita <gecko.alpad@gmail.com>
2020-06-24 07:23:57 +0300

    Removed Icon[CR].

--- stubs/scripts (1).js
+++ stubs/scripts (1).js
@@ -1,6 +1,6 @@
 window.onload = function () {
     window.onfocus = function () {
-        var input = document.querySelector('[aria-label="Type a message"]');
+        var input = document.querySelector('[aria-label="Type a message here"]');
         input && input.focus();
     };
 };
